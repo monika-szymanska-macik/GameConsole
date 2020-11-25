db84433d83a5200e3c722742f3b0f4b28606145d
Monika <m.szymanska.macik@gmail.com>
2020-11-25 22:55:07 +0100

    playerdisplayer class added

---
+++ GameConsole/GameConsole/PlayerDisplayer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameConsole
+{
+    class PlayerDisplayer
+    {
+        public static void Write(PlayerCharacter player)
+        {
+            Console.WriteLine(player.Name);
+            if(player.DaysSinceLastLogin == -1)
+            {
+                Console.WriteLine("No value for DaysSinceLastLogin");
+            }
+            else
+            {
+                Console.WriteLine(player.DaysSinceLastLogin);
+            }
+        }
+    }
+}
